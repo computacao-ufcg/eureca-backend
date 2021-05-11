1a6117e8debb20e94647a0c0154c43cf76c52be3
Jonatas Ferreira <ferreiradelimajonatas@gmail.com>
2021-05-11 11:44:12 -0300

    fixing some tests

--- src/test/java/br/edu/ufcg/computacao/eureca/backend/core/tests/models/StudentDataTest.java
+++ src/test/java/br/edu/ufcg/computacao/eureca/backend/core/tests/models/StudentDataTest.java
@@ -2,9 +2,9 @@ package br.edu.ufcg.computacao.eureca.backend.core.tests.models;
 
 import br.edu.ufcg.computacao.eureca.backend.core.models.StudentData;
 import br.edu.ufcg.computacao.eureca.backend.core.models.StudentStatus;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class StudentDataTest {
 
@@ -12,7 +12,7 @@ public class StudentDataTest {
     private StudentData studentDataDropout;
     private StudentData studentDataActive;
 
-    @BeforeEach
+    @Before
     public void setUp() {
         this.studentDataAlumnus = new StudentData();
         this.studentDataAlumnus.setStatusStr("Inativo (GRADUADO 2004.1)");
@@ -26,21 +26,21 @@ public class StudentDataTest {
 
     @Test
     public void testIsActive() {
-        Assertions.assertTrue(this.studentDataActive.isActive());
-        Assertions.assertEquals("Current", this.studentDataActive.getStatusTerm());
-        Assertions.assertEquals(StudentStatus.ACTIVE, this.studentDataActive.getStatus());
+        Assert.assertTrue(this.studentDataActive.isActive());
+        Assert.assertEquals("Current", this.studentDataActive.getStatusTerm());
+        Assert.assertEquals(StudentStatus.ACTIVE, this.studentDataActive.getStatus());
     }
 
     @Test
     public void testIsAlumnus() {
-        Assertions.assertTrue(this.studentDataAlumnus.isAlumnus());
-        Assertions.assertEquals(StudentStatus.ALUMNUS, this.studentDataAlumnus.getStatus());
+        Assert.assertTrue(this.studentDataAlumnus.isAlumnus());
+        Assert.assertEquals(StudentStatus.ALUMNUS, this.studentDataAlumnus.getStatus());
     }
 
     @Test
     public void testIsDropout() {
-        Assertions.assertTrue(this.studentDataDropout.isDropout());
-        Assertions.assertEquals(StudentStatus.DROPOUT, this.studentDataDropout.getStatus());
+        Assert.assertTrue(this.studentDataDropout.isDropout());
+        Assert.assertEquals(StudentStatus.DROPOUT, this.studentDataDropout.getStatus());
     }
 
     @Test
@@ -48,7 +48,7 @@ public class StudentDataTest {
         this.studentDataActive.setMandatoryCredits(10);
         this.studentDataActive.setElectiveCredits(10);
         this.studentDataActive.setComplementaryCredits(5); // case 1: complementary < 8
-        Assertions.assertEquals(25, this.studentDataActive.getCompletedCredits());
+        Assert.assertEquals(25, this.studentDataActive.getCompletedCredits());
     }
 
     @Test
@@ -56,12 +56,12 @@ public class StudentDataTest {
         this.studentDataActive.setMandatoryCredits(10);
         this.studentDataActive.setElectiveCredits(10);
         this.studentDataActive.setComplementaryCredits(10); // case 2: complementary > 8
-        Assertions.assertEquals(28, this.studentDataActive.getCompletedCredits());
+        Assert.assertEquals(28, this.studentDataActive.getCompletedCredits());
     }
 
     @Test
     public void testGetStatusIndex() {
-        Assertions.assertEquals(0, this.studentDataDropout.getStatusIndex());
-        Assertions.assertEquals(11, this.studentDataActive.getStatusIndex());
+        Assert.assertEquals(0, this.studentDataDropout.getStatusIndex());
+        Assert.assertEquals(11, this.studentDataActive.getStatusIndex());
     }
 }
