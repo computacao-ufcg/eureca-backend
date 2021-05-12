885ef1b9724a4b158c988f98b7413518a7e406e6
Jonatas Ferreira <ferreiradelimajonatas@gmail.com>
2021-05-12 18:54:48 -0300

    modeling TeacherSummaryResponse

--- src/main/java/br/edu/ufcg/computacao/eureca/backend/api/http/response/TeachersSummaryResponse.java
+++ src/main/java/br/edu/ufcg/computacao/eureca/backend/api/http/response/TeachersSummaryResponse.java
@@ -1,4 +1,64 @@
 package br.edu.ufcg.computacao.eureca.backend.api.http.response;
 
 public class TeachersSummaryResponse {
+
+    private MetricsSummary failedDueToAbsences;
+    private MetricsSummary failedDueToGrade;
+    private MetricsSummary failedDueToCanceling;
+    private MetricsSummary success;
+    private TermCount min;
+    private TermCount max;
+    private int total;
+
+    public TeachersSummaryResponse(MetricsSummary failedDueToAbsences, MetricsSummary failedDueToGrade, MetricsSummary failedDueToCanceling,
+                                   MetricsSummary success, TermCount min, TermCount max, int total) {
+        this.failedDueToAbsences = failedDueToAbsences;
+        this.failedDueToGrade = failedDueToGrade;
+        this.failedDueToCanceling = failedDueToCanceling;
+        this.success = success;
+        this.min = min;
+        this.max = max;
+        this.total = total;
+    }
+
+    public MetricsSummary getFailedDueToAbsences() {
+        return failedDueToAbsences;
+    }
+
+    public MetricsSummary getFailedDueToGrade() {
+        return failedDueToGrade;
+    }
+
+    public MetricsSummary getFailedDueToCanceling() {
+        return failedDueToCanceling;
+    }
+
+    public MetricsSummary getSuccess() {
+        return success;
+    }
+
+    public TermCount getMin() {
+        return min;
+    }
+
+    public TermCount getMax() {
+        return max;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    @Override
+    public String toString() {
+        return "TeachersSummaryResponse{" +
+                "failedDueToAbsences=" + failedDueToAbsences +
+                ", failedDueToGrade=" + failedDueToGrade +
+                ", failedDueToCanceling=" + failedDueToCanceling +
+                ", success=" + success +
+                ", min=" + min +
+                ", max=" + max +
+                ", total=" + total +
+                '}';
+    }
 }
---
+++ src/main/java/br/edu/ufcg/computacao/eureca/backend/api/http/response/TermCount.java
@@ -0,0 +1,28 @@
+package br.edu.ufcg.computacao.eureca.backend.api.http.response;
+
+public class TermCount {
+
+    private int count;
+    private String term;
+
+    public TermCount(int count, String term) {
+        this.count = count;
+        this.term = term;
+    }
+
+    public String getTerm() {
+        return term;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public String toString() {
+        return "TermCount{" +
+                "count=" + count +
+                ", term='" + term + '\'' +
+                '}';
+    }
+}
