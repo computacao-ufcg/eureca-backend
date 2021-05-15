e76a4e94b6f9ecddf11f43dd9cc6696dec0f3058
fubica <fubica@dsc.ufcg.edu.br>
2021-05-15 12:11:16 -0300

    Retry reading AS public key in case of failure

--- src/main/java/br/edu/ufcg/computacao/eureca/backend/core/holders/EurecaAsPublicKeyHolder.java
+++ src/main/java/br/edu/ufcg/computacao/eureca/backend/core/holders/EurecaAsPublicKeyHolder.java
@@ -2,13 +2,17 @@
 package br.edu.ufcg.computacao.eureca.backend.core.holders;
 
 import br.edu.ufcg.computacao.eureca.backend.constants.ConfigurationPropertyKeys;
+import br.edu.ufcg.computacao.eureca.backend.constants.Messages;
+import br.edu.ufcg.computacao.eureca.backend.core.ApplicationFacade;
 import br.edu.ufcg.computacao.eureca.common.exceptions.EurecaException;
 import br.edu.ufcg.computacao.eureca.common.util.PublicKeyUtil;
 import br.edu.ufcg.computacao.eureca.as.api.http.request.PublicKey;
+import org.apache.log4j.Logger;
 
 import java.security.interfaces.RSAPublicKey;
 
 public class EurecaAsPublicKeyHolder {
+    private static final Logger LOGGER = Logger.getLogger(EurecaAsPublicKeyHolder.class);
     private RSAPublicKey asPublicKey;
 
     private static EurecaAsPublicKeyHolder instance;
@@ -29,7 +33,13 @@ public class EurecaAsPublicKeyHolder {
             String asPort = null;
             asAddress = PropertiesHolder.getInstance().getProperty(ConfigurationPropertyKeys.AS_URL_KEY);
             asPort = PropertiesHolder.getInstance().getProperty(ConfigurationPropertyKeys.AS_PORT_KEY);
-            this.asPublicKey = PublicKeyUtil.getPublicKey(asAddress, asPort, PublicKey.ENDPOINT);
+            try {
+                this.asPublicKey = PublicKeyUtil.getPublicKey(asAddress, asPort, PublicKey.ENDPOINT);
+            } catch (EurecaException e) {
+                LOGGER.info(Messages.COULD_NOT_FETCH_AS_PUBLIC_KEY);
+                this.asPublicKey = null;
+                throw e;
+            }
         }
         return this.asPublicKey;
     }
