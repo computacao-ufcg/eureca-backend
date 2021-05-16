878c986ed4bd95d80ae17be1f675b1fe0c15c57c
fubica <fubica@dsc.ufcg.edu.br>
2021-05-16 12:53:04 -0300

    Fixing named inadvertedly changed in refactoring

--- src/main/java/br/edu/ufcg/computacao/eureca/backend/api/http/CommonKeys.java
+++ src/main/java/br/edu/ufcg/computacao/eureca/backend/api/http/CommonKeys.java
@@ -1,5 +1,5 @@
 package br.edu.ufcg.computacao.eureca.backend.api.http;
 
 public class CommonKeys {
-    public static final String AUTHENTICATION_TOKEN_KEY = "Authentication-TokenResponse";
+    public static final String AUTHENTICATION_TOKEN_KEY = "Authentication-Token";
 }
