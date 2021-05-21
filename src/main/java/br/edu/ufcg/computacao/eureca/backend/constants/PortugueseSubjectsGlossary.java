0fbb0a411484d0d583e747ac714559dab0f33efe
Natália Salvino <natalia.andre@ccc.ufcg.edu.br>
2021-05-21 00:39:38 -0300

    changing glossary names to portuguese

--- src/main/java/br/edu/ufcg/computacao/eureca/backend/constants/PortugueseSubjectsGlossary.java
+++ src/main/java/br/edu/ufcg/computacao/eureca/backend/constants/PortugueseSubjectsGlossary.java
@@ -3,10 +3,10 @@ package br.edu.ufcg.computacao.eureca.backend.constants;
 public class PortugueseSubjectsGlossary {
 
     public SubjectsGlossaryFields getGlossary() {
-        Field mandatory = new Field("mandatory", "");
-        Field optional = new Field("optional", "");
-        Field elective = new Field("elective", "");
-        Field complementary = new Field("complementary", "");
+        Field mandatory = new Field("Obrigatórias", "");
+        Field optional = new Field("Optativas", "");
+        Field elective = new Field("Eletivas", "");
+        Field complementary = new Field("Complementares", "");
 
         return new SubjectsGlossaryFields(mandatory, optional, elective, complementary);
     }
