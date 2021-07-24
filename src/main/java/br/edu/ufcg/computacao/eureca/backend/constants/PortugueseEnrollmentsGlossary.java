11036d8052c8b677298ae4c41fe6396c876d7fe5
iele passos <iele.passos@ccc.ufcg.edu.br>
2021-07-23 22:03:33 -0300

    change: adding descriptions to enrollments' glossary

--- src/main/java/br/edu/ufcg/computacao/eureca/backend/constants/PortugueseEnrollmentsGlossary.java
+++ src/main/java/br/edu/ufcg/computacao/eureca/backend/constants/PortugueseEnrollmentsGlossary.java
@@ -5,12 +5,14 @@ public class PortugueseEnrollmentsGlossary implements Glossary {
     @Override
     public EnrollmentsGlossaryFields getGlossary() {
         Field subjects = new Field("Disciplinas", "");
-        Field max = new Field("Número máximo", "");
-        Field min = new Field("Número mínimo", "");
-        Field averageClassesPerDiscipline = new Field("Média de turmas disciplina", "");
-        Field averageClassesPerPeriod = new Field("Média de turmas período", "");
-        Field averageEnrollmentsPerClass = new Field("Média de matrículas turma", "");
-        Field averageEnrollmentsPerPeriod = new Field("Média de matrículas período", "");
+        Field max = new Field("Número máximo", "Número máximo de matrículas por período considerando todos"+
+                "os períodos desde o início da operação do curso.");
+        Field min = new Field("Número mínimo", "Número mínimo de matrículas por período considerando todos"+
+                "os períodos desde o início da operação do curso.");
+        Field averageClassesPerDiscipline = new Field("Média turmas/disciplina", "Média de turmas por disciplina.");
+        Field averageClassesPerPeriod = new Field("Média de turmas/período", "Média de turmas por período.");
+        Field averageEnrollmentsPerClass = new Field("Média de matrículas/turma", "Média de matrículas por turma.");
+        Field averageEnrollmentsPerPeriod = new Field("Média de matrículas/período", "Média de matrículas por período.");
 
         return new EnrollmentsGlossaryFields(subjects, max, min, averageClassesPerDiscipline, averageClassesPerPeriod,
                 averageEnrollmentsPerClass, averageEnrollmentsPerPeriod);
