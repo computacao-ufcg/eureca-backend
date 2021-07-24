a97cac0835e5138c05011292155c386a701bdc20
iele passos <iele.passos@ccc.ufcg.edu.br>
2021-07-23 22:09:04 -0300

    change: adding descriptions to teachers' glossary

--- src/main/java/br/edu/ufcg/computacao/eureca/backend/constants/PortugueseTeachersGlossary.java
+++ src/main/java/br/edu/ufcg/computacao/eureca/backend/constants/PortugueseTeachersGlossary.java
@@ -3,12 +3,15 @@ package br.edu.ufcg.computacao.eureca.backend.constants;
 public class PortugueseTeachersGlossary implements Glossary {
 
     public TeachersGlossaryFields getGlossary() {
-        Field failedDueToAbsences =  new Field("Reprovações por falta", "");
-        Field failedDueToGrade = new Field("Reprovações por nota", "");;
-        Field failedDueToCanceling = new Field("Trancamentos", "");;
-        Field success = new Field("Taxa de Sucesso", "");
-        Field min = new Field("Número mínimo", "");
-        Field max = new Field("Número máximo", "");
+        Field failedDueToAbsences =  new Field("Reprovações/falta", "Média de reprovações por falta.");
+        Field failedDueToGrade = new Field("Reprovações/nota", "Média de reprovações por nota.");;
+        Field failedDueToCanceling = new Field("Trancamentos", "Média de trancamentos.");;
+        Field success = new Field("Taxa de Sucesso", "Essa métrica é calculada como a razão entre o número" +
+                "de créditos integralizados até o momento e o número agregado de créditos matriculados.");
+        Field min = new Field("Número mínimo", "Número mínimo de docentes por período considerando todos" +
+                "os períodos desde o início da operação do curso.");
+        Field max = new Field("Número máximo", "Número mánimo de docentes por período considerando todos" +
+                "os períodos desde o início da operação do curso.");
         Field total = new Field("Total", "");
 
         return new TeachersGlossaryFields(failedDueToAbsences, failedDueToGrade, failedDueToCanceling, success, min, max, total);
