aaaa353ddb293f4906c4607b270a2e7ead3ca258
Jonatas Ferreira <ferreiradelimajonatas@gmail.com>
2021-09-30 08:48:59 -0300

    feat: added 'maxCredits' query param to /pre_enrollment endpoint

--- src/main/java/br/edu/ufcg/computacao/eureca/backend/constants/ApiDocumentation.java
+++ src/main/java/br/edu/ufcg/computacao/eureca/backend/constants/ApiDocumentation.java
@@ -121,5 +121,6 @@ public class ApiDocumentation {
         public static final String GET_PRE_ENROLLMENT = "Retorna uma possível pré-matrícula de um aluno específico.";
         public static final String GET_PRE_ENROLLMENTS = "Retorna todas as pre-matriculas dos ativos e, juntamente, um sumario com a demanda de cada disciplina.";
         public static final String GET_DEMAND = "Retorna a demanda de cada disciplina, baseado na pré-matrícula calculada.";
+        public static final String MAX_CREDITS = "Número máximo de créditos na pré-matrícula.";
     }
 }
