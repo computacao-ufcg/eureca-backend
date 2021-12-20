e6b63f3ba6e06566817e20b893c7ef70bc58bd5b
Jonatas Ferreira <ferreiradelimajonatas@gmail.com>
2021-12-20 14:21:24 -0300

    fix: fix conflicts

--- src/test/java/br/edu/ufcg/computacao/eureca/backend/api/http/request/RetentionStatisticsTest.java
+++ src/test/java/br/edu/ufcg/computacao/eureca/backend/api/http/request/RetentionStatisticsTest.java
@@ -1,9 +1,5 @@
 package br.edu.ufcg.computacao.eureca.backend.api.http.request;
-import br.edu.ufcg.computacao.eureca.backend.api.http.response.active.ActivesStatisticsResponse;
-import br.edu.ufcg.computacao.eureca.backend.api.http.response.enrollment.EnrollmentsMetricsPerTerm;
-import br.edu.ufcg.computacao.eureca.backend.api.http.response.enrollment.EnrollmentsMetricsPerTermSummary;
-import br.edu.ufcg.computacao.eureca.backend.api.http.response.enrollment.EnrollmentsResponse;
-import br.edu.ufcg.computacao.eureca.backend.api.http.response.enrollment.EnrollmentsStatisticsResponse;
+
 import br.edu.ufcg.computacao.eureca.backend.api.http.response.retention.RetentionStatisticsSummaryResponse;
 import br.edu.ufcg.computacao.eureca.backend.api.http.response.retention.student.StudentsRetentionPerTermSummary;
 import br.edu.ufcg.computacao.eureca.backend.api.http.response.retention.student.StudentsRetentionStatisticsResponse;
@@ -13,15 +9,6 @@ import br.edu.ufcg.computacao.eureca.backend.api.http.response.retention.subject
 import br.edu.ufcg.computacao.eureca.backend.api.http.response.retention.subject.SubjectsRetentionStatisticsResponse;
 import br.edu.ufcg.computacao.eureca.backend.api.http.response.students.StudentMetrics;
 import br.edu.ufcg.computacao.eureca.backend.api.http.response.students.StudentMetricsSummary;
-import br.edu.ufcg.computacao.eureca.backend.api.http.response.students.StudentsStatisticsSummaryResponse;
-import br.edu.ufcg.computacao.eureca.backend.api.http.response.subject.SubjectMetricsStatistics;
-import br.edu.ufcg.computacao.eureca.backend.api.http.response.subject.SubjectsStatisticsSummary;
-import br.edu.ufcg.computacao.eureca.backend.api.http.response.subject.SubjectsStatisticsSummaryResponse;
-import br.edu.ufcg.computacao.eureca.backend.api.http.response.teacher.TeachersStatisticsSummary;
-import br.edu.ufcg.computacao.eureca.backend.api.http.response.teacher.TeachersStatisticsSummaryResponse;
-import br.edu.ufcg.computacao.eureca.backend.core.models.MetricStatistics;
-import br.edu.ufcg.computacao.eureca.backend.core.models.SubjectType;
-import br.edu.ufcg.computacao.eureca.backend.core.models.TermCount;
 import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -65,7 +52,6 @@ public class RetentionStatisticsTest extends EndpointTest {
         Mockito.doReturn(response).when(this.facade).getSubjectsRetentionStatistics(Mockito.anyString(), Mockito.anyString(), Mockito.anyString(), Mockito.anyString(), Mockito.anyString());
         RequestBuilder req = this.getRequestBuilder(HttpMethod.GET, RETENTION_STATISTICS_ENDPOINT + "/subjects" + DEFAULT_COURSE_CURRICULUM_QUERY);
 
-
         // exercise
         MvcResult res = this.mockMvc.perform(req).andReturn();
 
@@ -79,7 +65,7 @@ public class RetentionStatisticsTest extends EndpointTest {
         // set up
         SubjectsRetentionResponse response = getSubjectsRetentionCsvResponse();
         Mockito.doReturn(response).when(this.facade).getSubjectsRetentionCSV(Mockito.anyString(), Mockito.anyString(), Mockito.anyString(), Mockito.anyString(), Mockito.anyString());
-        RequestBuilder req = this.getRequestBuilder(HttpMethod.GET, RETENTION_STATISTICS_ENDPOINT + "/subjects/csv" + DEFAULT_COURSE_CURRICULUM_QUERY, null, "");
+        RequestBuilder req = this.getRequestBuilder(HttpMethod.GET, RETENTION_STATISTICS_ENDPOINT + "/subjects/csv" + DEFAULT_COURSE_CURRICULUM_QUERY);
 
         // exercise
         MvcResult res = this.mockMvc.perform(req).andReturn();
@@ -95,7 +81,7 @@ public class RetentionStatisticsTest extends EndpointTest {
         RetentionStatisticsSummaryResponse response = new RetentionStatisticsSummaryResponse("","",null,null);
         Mockito.doReturn(response).when(this.facade).getRetentionStatisticsSummary(Mockito.anyString(), Mockito.anyString(), Mockito.anyString(), Mockito.anyString(), Mockito.anyString(), Mockito.anyString());
 
-        RequestBuilder req = this.getRequestBuilder(HttpMethod.GET, RETENTION_STATISTICS_ENDPOINT + "/summary" + DEFAULT_COURSE_CURRICULUM_QUERY, null, "");
+        RequestBuilder req = this.getRequestBuilder(HttpMethod.GET, RETENTION_STATISTICS_ENDPOINT + "/summary" + DEFAULT_COURSE_CURRICULUM_QUERY);
         MvcResult res = this.mockMvc.perform(req).andReturn();
 
         Assert.assertEquals(HttpStatus.OK.value(), res.getResponse().getStatus());
