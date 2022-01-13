21a4d453cbe041c38de42975d67341c2a1f9cd59
Jonatas Ferreira <ferreiradelimajonatas@gmail.com>
2022-01-13 17:11:55 -0300

    fix: change exceptions extension from EurecaException to InvalidParameterException

--- src/main/java/br/edu/ufcg/computacao/eureca/backend/api/http/exceptions/pre_enrollment/StudentNotFoundException.java
+++ src/main/java/br/edu/ufcg/computacao/eureca/backend/api/http/exceptions/pre_enrollment/StudentNotFoundException.java
@@ -1,9 +1,9 @@
 package br.edu.ufcg.computacao.eureca.backend.api.http.exceptions.pre_enrollment;
 
 import br.edu.ufcg.computacao.eureca.backend.constants.Messages;
-import br.edu.ufcg.computacao.eureca.common.exceptions.EurecaException;
+import br.edu.ufcg.computacao.eureca.common.exceptions.InvalidParameterException;
 
-public class StudentNotFoundException extends EurecaException {
+public class StudentNotFoundException extends InvalidParameterException {
 
     public StudentNotFoundException(String registration) {
         super(String.format(Messages.STUDENT_S_NOT_FOUND, registration));
