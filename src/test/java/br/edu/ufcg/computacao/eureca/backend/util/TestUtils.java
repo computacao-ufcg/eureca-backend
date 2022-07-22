b7dcc71307340393c43a29eec16cb38f851e7b54
Maria Costa <maria.costa@youxgroup.com.br>
2022-07-21 21:54:05 -0300

    Removendo primeiro bad smell - construtor com muitos métodos / criando nova classe que agrupa taxas médias

--- src/test/java/br/edu/ufcg/computacao/eureca/backend/util/TestUtils.java
+++ src/test/java/br/edu/ufcg/computacao/eureca/backend/util/TestUtils.java
@@ -19,6 +19,7 @@ import br.edu.ufcg.computacao.eureca.backend.api.http.response.subject.SubjectCS
 import br.edu.ufcg.computacao.eureca.backend.api.http.response.subject.SubjectMetrics;
 import br.edu.ufcg.computacao.eureca.backend.api.http.response.subject.SubjectsResponse;
 import br.edu.ufcg.computacao.eureca.backend.api.http.response.teacher.*;
+import br.edu.ufcg.computacao.eureca.backend.api.http.response.teacher.refator.AverageRates;
 import br.edu.ufcg.computacao.eureca.backend.constants.TeachersGlossaryFields;
 import br.edu.ufcg.computacao.eureca.backend.core.dao.scsvfiles.mapentries.EnrollmentData;
 import br.edu.ufcg.computacao.eureca.backend.api.http.response.students.StudentCSV;
@@ -222,8 +223,9 @@ public class TestUtils {
         TermCount termCount1 = new TermCount(0,"2019.1");
         TermCount termCount2 = new TermCount(0, "2020.1");
         Map<String, TeachersStatisticsSummary> map = new HashMap<>();
+        AverageRates averageRates = new AverageRates(0,0,0,0);
         TeachersStatisticsSummary teachersStatisticsSummary = new TeachersStatisticsSummary("2018.1","2020.1",0,termCount1,
-                termCount2,0,0,0,0);
+                termCount2, averageRates);
 
         map.put("x", teachersStatisticsSummary);
         return new TeachersStatisticsSummaryResponse("", "", map);
